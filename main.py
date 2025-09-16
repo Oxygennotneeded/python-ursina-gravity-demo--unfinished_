15efc91b91f4d9d0c373369a986f13e9813258c8
Oxygennotneeded <mingaygeorge40@gmail.com>
2025-09-16 14:50:19 +0100

    added camera moving and planets for demo

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from ursina import *
+import planet
 
+#---USER SETTINGS-------
+hold_duration_threshold = 0.25
+
+#---Boolean values----
+
+
+#--Ursina shit
 app = Ursina()
+EditorCamera() 
+
+num_planets = 2
+planets_positions = [Vec3(0,0,0), Vec3(1,0,0)]
+planet_list = []
+
+for i in range(num_planets):
+    new_planet = planet.PLANET(position=planets_positions[i])
+    planet_list.append(new_planet)
 
 
 
+def update():
+
+    camera.x += held_keys['d'] * time.dt
+    camera.x -= held_keys['a'] * time.dt
+    camera.z += held_keys['w'] * time.dt
+    camera.z -= held_keys['s'] * time.dt
+    
+    
 app.run()
+#all the cool shit
---
+++ planet.py
@@ -0,0 +1,12 @@
+from ursina import *
+
+class PLANET(Entity):
+
+    def __init__(self, planet_color=color.orange, position = (0,0,0), scale = 1):
+        super().__init__(
+            model='sphere',
+            texture='white_cube',
+            color= planet_color,
+            position=position,
+            scale=scale
+        )
